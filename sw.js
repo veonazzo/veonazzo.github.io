ca683eaa225e113030ad27774a66897e26ef7e83
Andrea Masala <veonazzo@shardansoft.space>
2021-02-23 21:48:36 +0100

    new build some fix

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a1c076ca0af28a5f7e2c.js"
+    "url": "webpack-runtime-25ea56faf7a46639d626.js"
   },
   {
     "url": "framework-dab657cf2d102cb8d2b9.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "73118824cf8f31cacd910dbf77127206"
+    "revision": "4a2bf97873a34d8526766d3678189267"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b81c43cf3b4ef9854106.js"
